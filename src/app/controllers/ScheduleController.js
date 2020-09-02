5cab00ff1d4d0ee7d5ec094801804f018133eac6
Tadeu Tupinambá <tadeu.tupiz@gmail.com>
2020-09-01 22:22:05 -0300

    lint(pepsico): lint some files and use prettier

--- src/app/controllers/ScheduleController.js
+++ src/app/controllers/ScheduleController.js
@@ -4,43 +4,40 @@ import User from '../models/User';
 import Appointment from '../models/Appointment';
 
 class ScheduleController {
-    async index(req, res) {
-        const checkUserProvider = await User.findOne({
-            where: { id: req.userId, provider: true },
-        });
+  async index(req, res) {
+    const checkUserProvider = await User.findOne({
+      where: { id: req.userId, provider: true },
+    });
 
-        if (!checkUserProvider) {
-            return res.status(401).json({
-                error: 'User is not a provider',
-            });
-        }
-
-        const { date } = req.query;
-        const parsedDate = parseISO(date);
-
-        const appointments = await Appointment.findAll({
-            where: {
-                provider_id: req.userId,
-                canceled_at: null,
-                date: {
-                    [Op.between]: [
-                        startOfDay(parsedDate),
-                        endOfDay(parsedDate),
-                    ],
-                },
-            },
-            include: [
-                {
-                    model: User,
-                    as: 'user',
-                    attributes: ['name'],
-                },
-            ],
-            order: ['date'],
-        });
-
-        return res.json(appointments);
+    if (!checkUserProvider) {
+      return res.status(401).json({
+        error: 'User is not a provider',
+      });
     }
+
+    const { date } = req.query;
+    const parsedDate = parseISO(date);
+
+    const appointments = await Appointment.findAll({
+      where: {
+        provider_id: req.userId,
+        canceled_at: null,
+        date: {
+          [Op.between]: [startOfDay(parsedDate), endOfDay(parsedDate)],
+        },
+      },
+      include: [
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name'],
+        },
+      ],
+      order: ['date'],
+    });
+
+    return res.json(appointments);
+  }
 }
 
 export default new ScheduleController();
--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -6,52 +6,52 @@ import File from '../models/File';
 import authConfig from '../../config/auth';
 
 class SessionController {
-    async store(req, res) {
-        const schema = Yup.object().shape({
-            email: Yup.string()
-                .email()
-                .required(),
-            password: Yup.string()
-                .min(3)
-                .required(),
-        });
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string()
+        .min(3)
+        .required(),
+    });
 
-        if (!(await schema.isValid(req.body))) {
-            res.status(422).json({ error: 'Validation fails' });
-        }
-
-        const { email, password } = req.body;
-
-        const user = await User.findOne({
-            where: { email },
-            include: [
-                {
-                    model: File,
-                    as: 'avatar',
-                    attributes: ['id', 'path', 'url'],
-                },
-            ],
-        });
-
-        if (!user) {
-            return res.status(401).json({ error: 'User not found' });
-        }
-
-        if (!user.checkPassword(password)) {
-            return res.status(403).json({ error: 'Password does not match' });
-        }
-
-        const { id, name, avatar, provider } = user;
-
-        const token = jwt.sign({ id, email }, authConfig.secret, {
-            expiresIn: authConfig.expiresIn,
-        });
-
-        return res.json({
-            user: { id, name, email, avatar, provider },
-            token,
-        });
+    if (!(await schema.isValid(req.body))) {
+      res.status(422).json({ error: 'Validation fails' });
     }
+
+    const { email, password } = req.body;
+
+    const user = await User.findOne({
+      where: { email },
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'path', 'url'],
+        },
+      ],
+    });
+
+    if (!user) {
+      return res.status(401).json({ error: 'User not found' });
+    }
+
+    if (!user.checkPassword(password)) {
+      return res.status(403).json({ error: 'Password does not match' });
+    }
+
+    const { id, name, avatar, provider } = user;
+
+    const token = jwt.sign({ id, email }, authConfig.secret, {
+      expiresIn: authConfig.expiresIn,
+    });
+
+    return res.json({
+      user: { id, name, email, avatar, provider },
+      token,
+    });
+  }
 }
 
 export default new SessionController();
